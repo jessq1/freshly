2cd65c3cda2775aa3e0111fa7854e4223b4c3519
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-23 12:46:36 -0400

    establish fridge freezer list links

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,4 +1,5 @@
 import { Profile } from '../models/profile.js'
+import { User } from '../models/user.js'
 
 export {
     show,
@@ -7,8 +8,16 @@ export {
     showList,
 }
 
+// function show(req, res) {
+//   console.log(req.params.id)
+//   res.render('fridge/show', {
+//     title: 'My Fridge'
+//   })
+// }
+
   function show(req, res) {
-    Profile.find(req.params.id, function(err, Profile) {
+    Profile.findById(req.params.id, function(err, Profile) {
+      // console.log(Profile)
       res.render('fridge/show', {
         Profile: Profile,
         title: 'My Fridge'
@@ -16,11 +25,26 @@ export {
     })
   }
   function showFridge(req, res) {
-
+    Profile.findById(req.params.id, function(err, Profile) {
+      res.render('fridge/showFridge', {
+        Profile: Profile,
+        title: 'My Fridge'
+      })
+    })
   }
   function showFreezer(req, res) {
-    
+    Profile.findById(req.params.id, function(err, Profile) {
+      res.render('fridge/showFreezer', {
+        Profile: Profile,
+        title: 'My Fridge'
+      })
+    })
   }
   function showList(req, res) {
-    
+    Profile.findById(req.params.id, function(err, Profile) {
+      res.render('fridge/showList', {
+        Profile: Profile,
+        title: 'My Fridge'
+      })
+    })
   }
