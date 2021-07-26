8da4230cc8814c2b8f466c220cce974c04a83295
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-25 21:02:07 -0600

    adjust add to list function

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -39,11 +39,52 @@ export {
       })
     })
   }
+
   function showList(req, res) {
-    Profile.findById(req.params.id, function(err, Profile) {
-      res.render('fridge/showList', {
-        Profile: Profile,
-        title: 'My Shopping List'
-      })
-    })
+    // console.log(Profile.findById(req.params.id))
+  //   Profile.
+  //   findById(req.params.id).
+  //   populate({
+  //   path: 'list',
+  //   populate: { path: 'food' }
+  // }).exec(function(err, profile){
+  //   res.render('fridge/showList', {
+  //           title: 'My Shopping List', 
+  //           profile: profile,
+  //           food: food
+  // })})
+  Profile
+.findById(req.params.id)
+.populate({
+  path: 'list', 
+  model: 'list',
+  populate: {
+    path: 'food',
+    model: 'Food'
+  }
+})
+.exec(function(err, profile){
+  res.render('fridge/showList', {
+          title: 'My Shopping List', 
+          profile: profile,
+        })
+});
+    // Profile.findById(req.params.id).list.
+    // Profile.findById(req.params.id).list[0]
+    // .populate('food').exec(function(err, profile) {
+    //   Food.find({_id: {$nin: profile.list[0].food}}, function(err, food) {
+    //     res.render('fridge/showList', {
+    //       title: 'My Shopping List', 
+    //       profile: profile,
+    //       food: food
+    //     })
+    //   })
+    // })
+
+    // Profile.findById(req.params.id, function(err, Profile) {
+    //   res.render('fridge/showList', {
+    //     Profile: Profile,
+    //     title: 'My Shopping List'
+    //   })
+    // })
   }
