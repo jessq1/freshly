576076f2b6495bb867fd94b987c945e0273cbf86
JessQi <85958041+jessq1@users.noreply.github.com>
2021-07-26 11:40:43 -0600

    add food to fridge

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -70,25 +70,25 @@ export {
   }
 
   function submitList(req, res) {
-    console.log(req.body.inFridge)
-//need to map food id
-    // if(req.body.inFridge == 'true'){
-    //   Profile
-    // .findById(req.params.id)
-    // .populate({
-    // path: 'list', 
-    // model: 'list',
-    // })
-    // .exec(function(err, profile){
-    //   profile.fridge.food.push(req.body)
-    //   console.log(profile.fridge.food)
-    //   profile.save(function(err) {
-    //     res.redirect(`/myfridge/${profile._id}/list`)
-    //   })
-    // });
-    // } else {
-    //   console.log(req.body)
-    // }
+    // console.log(req.body.foodId)
+
+    if(req.body.inFridge == 'true'){
+      Profile
+    .findById(req.params.id)
+    .populate({
+    path: 'list', 
+    model: 'list',
+    })
+    .exec(function(err, profile){
+      profile.fridgeFood.push(req.body.foodId)
+      console.log(profile.fridgeFood)
+      profile.save(function(err) {
+        res.redirect(`/myfridge/${profile._id}/list`)
+      })
+    });
+    } else {
+      console.log(req.body)
+    }
 
     // Profile.findById(req.params.id, function(err, profile) {
     //   profile.food.push(req.body)
